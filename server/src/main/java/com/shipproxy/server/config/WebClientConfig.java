9172aa171dbe53feda61bb23cc358d1f6d7bbcd8
cptblackbeard <indrajithvinodnair@gmail.com>
2025-03-21 03:09:02 +0530

    reverted changes to use default configurations for webclient as the server will need to maintain multiple TCP connections with other ships probably

--- server/src/main/java/com/shipproxy/server/config/WebClientConfig.java
+++ server/src/main/java/com/shipproxy/server/config/WebClientConfig.java
@@ -2,16 +2,12 @@ package com.shipproxy.server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
     @Bean
     public WebClient webClient(WebClient.Builder builder) {
-        return builder
-                .defaultHeader(HttpHeaders.USER_AGENT, "Spring WebClient")
-                .build();
+        return builder.build(); // Default connection pool
     }
-
 }
