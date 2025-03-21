9bba9e3ffaa855b3e213e96dc500792304612c39
cptblackbeard <indrajithvinodnair@gmail.com>
2025-03-21 12:58:19 +0530

    Ditched lombok as it was causing build issues

--- client/src/main/java/com/shipproxy/client/ProxyRequest.java
+++ client/src/main/java/com/shipproxy/client/ProxyRequest.java
@@ -1,16 +1,43 @@
 package com.shipproxy.client;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 
-@Data
-@AllArgsConstructor
 public class ProxyRequest {
     private String url;
     private String body;
     private HttpHeaders headers;
     private HttpMethod httpMethod;
     private String requestId;
+
+    public ProxyRequest(String url, String body, HttpHeaders headers, HttpMethod httpMethod, String requestId) {
+        this.url = url;
+        this.body = body;
+        this.headers = headers;
+        this.httpMethod = httpMethod;
+        this.requestId = requestId;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public HttpHeaders getHeaders() {
+        return headers;
+    }
+
+    public HttpMethod getHttpMethod() {
+        return httpMethod;
+    }
+
+    public String getRequestId() {
+        return requestId;
+    }
+    
+
 }
